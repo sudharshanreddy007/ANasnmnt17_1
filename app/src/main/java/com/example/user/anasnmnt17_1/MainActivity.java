ca6d9110d46eb391094576f3d7a658e1d3e484f7
sudharshanreddy007 <31057445+sudharshanreddy007@users.noreply.github.com>
2018-01-19 14:38:10 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/user/anasnmnt17_1/MainActivity.java
+++ app/src/main/java/com/example/user/anasnmnt17_1/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.user.anasnmnt17_1;
 
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,6 +16,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     //declaring widgets variables
     Button startButton, stopButton;
     MediaPlayer myPlayer;
+    NotificationCompat.Builder builder;
 
     //onCreate method is called by  creating main activity
     @Override
@@ -21,6 +25,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 //implementing button id's from the layout xml file which alocates the id
         startButton = (Button) findViewById(R.id.start_button);
+        Intent intent= new Intent(MainActivity.this,MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this,1,intent,PendingIntent.FLAG_UPDATE_CURRENT);
+
+         builder= new NotificationCompat.Builder(MainActivity.this);
+        builder.setContentTitle("music player");
+        builder.setContentText("playing music");
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setContentIntent(pendingIntent);
         myPlayer = MediaPlayer.create(MainActivity.this,R.raw.song);
         if(myPlayer.isPlaying()){
             Toast.makeText(this, "playing.....", Toast.LENGTH_SHORT).show();
@@ -39,15 +51,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (v.getId()) {
             case R.id.start_button:// Play audio from the start service intent of context MyMusic.class
               // Set looping
+             //   myPlayer.setLooping(true);
                myPlayer.start();
+                NotificationManager notificationManager= (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+                notificationManager.notify(2,builder.build());
                 Toast.makeText(this, " Music Started", Toast.LENGTH_LONG).show();
+
                 break;
             case R.id.stop_button:// Pause audio
              myPlayer.stop();
+              //  myPlayer.setLooping(false);
                 Toast.makeText(this, " Music Stopped", Toast.LENGTH_LONG).show();
                 break;
         }
     }
+
+
 
     @Override
     protected void onStop() {
